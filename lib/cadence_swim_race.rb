4028c1c16710443c42631facc30e01c477a98f20
John Neusteter <jneusteter@gmail.com>
2018-12-12 07:25:00 -0500

    add swim record

--- lib/cadence_swim_race.rb
+++ lib/cadence_swim_race.rb
@@ -96,6 +96,9 @@ class CadenceSwimRace
       { meet: 'Expressway Lincoln Fall Invitational', date: '2018/11/18', course: 25, style: 'free', distants: 50, time: '33.09', pos: 5 },
       { meet: 'Expressway Lincoln Fall Invitational', date: '2018/11/18', course: 25, style: 'free', distants: 200, time: '2:50.40', pos: 5 },
       { meet: 'Expressway Lincoln Fall Invitational', date: '2018/11/18', course: 25, style: 'back', distants: 100, time: '1:29.34', pos: 10 },
+      { meet: 'Aqua 7 STJJ vs NHAC', date: '2018/12/8', course: 25, style: 'free', distants: 100, time: '1:13.69', pos: 1 },
+      { meet: 'Aqua 7 STJJ vs NHAC', date: '2018/12/8', course: 25, style: 'back', distants: 100, time: '1:25.60', pos: 1 },
+      { meet: 'Aqua 7 STJJ vs NHAC', date: '2018/12/8', course: 25, style: 'fly', distants: 50, time: '42.73', pos: 1 },
     ]
   end
 
