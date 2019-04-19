6e4cd72304218d5a1fa1f031d55bedd485273b6c
John Neusteter <jneusteter@gmail.com>
2019-04-19 12:34:16 -0400

    modify photography page

---
+++ lib/filters/thumbnailize.rb
@@ -0,0 +1,8 @@
+class Thumbnailize < Nanoc::Filter
+  identifier  :thumbnailize
+  type        :binary
+
+  def run(filename, params = {})
+    system('convert', '-resize', params[:width].to_s, filename, output_filename)
+  end
+end
