f1fff956176dbca675195e9a8057052da5b3c71f
John Neusteter <jneusteter@gmail.com>
2019-04-21 10:17:35 -0400

    add vue

---
+++ src/components/ImageModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <a :href="imgPath">
+      <img :src="src" :alt="alt">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imgPath: {
+      type: String,
+      required: true
+    },
+    src: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,31 @@
-const path = require("path");
+const path = require('path')
+
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-  entry: "./src/main.js",
+  entry: './src/main.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "content/js")
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'content/js')
   },
   module: {
     rules: [
       {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
         test: /\.js$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/
       }
     ]
-  }
-};
+  },
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  plugins: [new VueLoaderPlugin()]
+}
