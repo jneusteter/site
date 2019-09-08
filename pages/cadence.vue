56f470f313e7e3b0906a47765ced52fdadad94ad
John Neusteter <jneusteter@gmail.com>
2019-09-08 10:21:44 -0400

    add cadence page

---
+++ pages/cadence.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div v-for="time in cadenceSwimTimes" :key="time.id">{{ time }}</div>
+  </div>
+</template>
+
+<script>
+import SwimTimes from '../components/data/cadence_swim_times.json'
+export default {
+  data() {
+    return {
+      cadenceSwimTimes: SwimTimes
+    }
+  }
+}
+</script>
