a7dcb35dc78699fb7670c172f20f0528bbe43bac
John Neusteter <jneusteter@gmail.com>
2019-09-22 10:20:50 -0400

    update cadence page

---
+++ components/SwimTimeTable.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <span v-if="eventsExist">
+      <h2>
+        {{ `${events[0].distance}M ${events[0].style} (${course})` }}
+      </h2>
+      <div class="table-container">
+        <table>
+          <thead>
+            <th>Meet</th>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Position</th>
+          </thead>
+          <tbody>
+            <tr v-for="event in events" :key="event.time">
+              <td>{{ event.meet }}</td>
+              <td>{{ event.date }}</td>
+              <td>{{ event.time }}</td>
+              <td>{{ event.pos }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    events: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    eventsExist() {
+      if (this.events.length === 0) {
+        return false
+      } else {
+        return true
+      }
+    },
+    course() {
+      if (this.events[0].course === 25) {
+        return 'SC'
+      } else {
+        return 'LC'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.table-container {
+  background: linear-gradient(-68deg, #ac32e4, #4801ff);
+  border-radius: .1rem;
+  font-family: sans-serif;
+}
+table {
+  width: 100%;
+  border-collapse: collapse;
+  color: white;
+}
+th {
+  background-color: rbga(255,255,255,0.32);
+  text-transform: uppercase;
+  padding-top: 1.2rem;
+  padding-bottom: 1.2rem;
+}
+td {
+  padding: 0.5rem;
+}
+tr:hover {
+  background: rbga(255,255,255,0.1);
+}
+/* table, td, th {
+  border: 1px solid black;
+} */
+</style>
--- pages/cadence.vue
+++ pages/cadence.vue
@@ -1,29 +1,69 @@
 <template>
   <div class="container">
     <h1>Cadence Neusteter</h1>
+    <hr />
     <div>
       <img src="/img/cadence.jpg" alt="Cadence Neusteter" />
     </div>
-    <div>
-      <div v-for="time in cadenceSwimTimes" :key="time.id">{{ time }}</div>
+    <div v-for="style in styles" :key="style.id">
+      <div v-for="distance in distances" :key="distance.id">
+        <SwimTimeTable :events="events(distance, style, 25)" />
+        <SwimTimeTable :events="events(distance, style, 50)" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import SwimTimes from '../components/data/cadence_swim_times.json'
+import swimEvents from '../components/data/cadence_swim_events.json'
+import SwimTimeTable from '../components/SwimTimeTable.vue'
+
 export default {
+  components: {
+    SwimTimeTable
+  },
   data() {
     return {
-      cadenceSwimTimes: SwimTimes
+      cadenceSwimEvents: swimEvents
+    }
+  },
+  computed: {
+    styles() {
+      const styles = []
+      this.cadenceSwimEvents.forEach((event) => {
+        styles.push(event.style)
+      })
+      return [...new Set(styles)]
+    },
+    distances() {
+      const distances = []
+      this.cadenceSwimEvents.forEach((event) => {
+        distances.push(event.distance)
+      })
+      return [...new Set(distances)]
+    }
+  },
+  methods: {
+    events(distance, style, course) {
+      const array = this.cadenceSwimEvents.filter(
+        (event) =>
+          event.style === style &&
+          event.distance === distance &&
+          event.course === course
+      )
+      return array.sort((a, b) => {
+        return new Date(b.date) - new Date(a.date)
+      })
     }
   }
 }
 </script>
 
 <style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
+hr {
+  height: 0.8rem;
+  border: 0rem;
+  width: 100%;
+  box-shadow: 0 10px 10px -10px #ac32e4 inset;
 }
 </style>
