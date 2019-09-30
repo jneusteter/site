57e6f696ce63ad88932c22f9eaad640c223212e3
John Neusteter <jneusteter@gmail.com>
2019-09-30 18:32:49 -0400

    navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="flex items-center justify-between px-4 py-3 bg-gray-900">
+    <div class="text-white text-xl">
+      jneusteter
+    </div>
+    <div>
+      <button type="button" class="block text-grey-600">
+        <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+          <path
+            fill-rule="evenodd"
+            d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+          />
+          <path
+            fill-rule="evenodd"
+            d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+          />
+        </svg>
+      </button>
+    </div>
+  </header>
+</template>
